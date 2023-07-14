3d06467dd3e64f770e0e1fdf548faf8c510ba393
dpftlel21 <dkdnmju@naver.com>
2023-07-14 14:46:26 +0900

    상품 리스트 불러오기 및 배열 완료

---
+++ src/components/styles/ProductsListsStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ProductsListsContainer = styled.div`
+  width: 80%;
+  height: 615px;
+  margin-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  overflow-y: scroll;
+`;
--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,10 +1,40 @@
 import React from "react";
 import  NavButton  from "../components/NavButton";
+import { ProductsListsContainer } from "../components/styles/ProductsListsStyle";
+import RenderItem from "../components/RenderItem";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import Modal from "../components/Modal";
 
 function ProductsLists() {
+
+  const [products, setProducts] = useState([]);
+  const [selectedProductId, setSelectedProductId] = useState(null);
+  const { ref, inView } = useInView({
+    threshold: 0
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://cozshopping.codestates-seb.link/api/v1/products?")
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
   return (
     <>  
         <NavButton />
+        <ProductsListsContainer ref={ref} >
+          <RenderItem products={products} inView={inView} setSelectedProductId={setSelectedProductId} selectedProductId={selectedProductId}/>
+          <Modal products={products} setSelectedProductId={setSelectedProductId} selectedProductId={selectedProductId}/>
+        </ProductsListsContainer>
     </>
   );
 }
